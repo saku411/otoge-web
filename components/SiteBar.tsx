13348443672c231f98bf52e41f0c4934d1a241fe
ryone9re <ryoichi.kurimoto@gmail.com>
2022-01-22 18:20:26 +0900

    ディレクトリの変更

---
+++ components/SiteBar.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const appBarLabel = (label: string) => {
+  return (
+    <Toolbar>
+      <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1 }}>
+        {label}
+      </Typography>
+      <IconButton edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+        <MenuIcon />
+      </IconButton>
+    </Toolbar>
+  )
+}
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+})
+
+const SiteBar = () => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position='static' color='primary'>
+        {appBarLabel('音ゲー')}
+      </AppBar>
+    </ThemeProvider>
+  )
+}
+
+export default SiteBar
