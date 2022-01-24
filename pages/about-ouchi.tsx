74c8b999d6fbf008c1de79d08658033bf8f897df
ryone9re <ryoichi.kurimoto@gmail.com>
2022-01-24 18:46:52 +0900

    ページの追加 #13 #14 #15 #16

---
+++ pages/about-ouchi.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AboutOuchi = () => {
+  return <h1>about-ouchi</h1>
+}
+
+export default AboutOuchi
---
+++ pages/howto.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HowTo = () => {
+  return <h1>howto</h1>
+}
+
+export default HowTo
