9b56a54ca138704dda6bdbafeee5789883af6e67
saku411 <vip8124@yahoo.co.jp>
2022-02-18 18:43:07 +0900

    ページ作成

--- libs/KadaiKyokuLists.ts
+++ libs/KadaiKyokuLists.ts
@@ -5,188 +5,188 @@ export type KadaiKyokuList = {
   remark: string
 }
 
-const KadaiKyokuLists: KadaiKyokuList[] = [
+export const KadaiKyokuLists: KadaiKyokuList[] = [
   {
     title: 'QQQ',
-    image: '/public/kadai-song/iidx/lt_iidx29_title.jpg',
+    image: '/kadai-song/iidx/lt_iidx29_title.jpg',
     difficulty: '10',
     remark: 'テクくてムズいやつにしました',
   },
   {
     title: 'キャトられ♥恋はモ～モク',
-    image: '/public/kadai-song/iidx/lt_iidx29_title.jpg',
+    image: '/kadai-song/iidx/lt_iidx29_title.jpg',
     difficulty: '12',
     remark: 'テクくてムズいやつにしました',
   },
   {
     title: 'Raspberry Railgun',
-    image: '/public/kadai-song/iidx/lt_iidx29_title.jpg',
+    image: '/kadai-song/iidx/lt_iidx29_title.jpg',
     difficulty: '10',
     remark: '曲がイイ！',
   },
   {
     title: 'BLUST OF WIND',
-    image: '/public/kadai-song/iidx/lt_iidx29_title.jpg',
+    image: '/kadai-song/iidx/lt_iidx29_title.jpg',
     difficulty: '10',
     remark: '曲がいい！',
   },
   {
     title: 'Iridescent Clouds',
-    image: '/public/kadai-song/sdvx/Iridescent Clouds.png',
+    image: '/kadai-song/sdvx/Iridescent Clouds.png',
     difficulty: '17',
     remark:
       'まず1曲目ですが、去年よりもやや難易度高めのPUC難易度の曲を選んで欲しいとのことだったので、MXMに関しては去年のultra turboがPUC難易度17.4(非公式難易度表より)だった事を考慮して、難易度17.5の曲を選ばせていただきました。また、EXHについてもLv14にしては若干難しめな譜面である為、昨年の課題曲からの難化を目指して、この曲を選びました。自分の話にはなるんですが、当然譜面も曲も大好きなのですが、実はその中でもジャケットが大好きなんです。曲名の和訳は「彩雲」なのですが、そのイメージをまさしく表現していて本当に凄いと思います。「スティンガー先生ってこんな綺麗な背景画も描けるんだ…」というような視点も、スコア粘着の際には是非持っていただけたらなと思います',
   },
   {
     title: 'Turn the story',
-    image: '/public/kadai-song/sdvx/Turn the story.png',
+    image: '/kadai-song/sdvx/Turn the story.png',
     difficulty: '18',
     remark:
       '2曲目に関しては、1曲目と同じように昨年からの難化を意識して、MXMに関しては昨年のGaLaXyEggPlanTがPUC難易度18.4(非公式難易度表より)だった為、PUC難易度18.8の曲を選ばせていただきました。また、EXHについても、Lv16適正レベルの人が楽しく叩ける鍵盤譜面であると判断し、選曲しました。何故数ある18上位譜面の中でこの曲を選んだかというと、PUC難易度とSやAAA難易度の乖離が激しいところにあります、この譜面は数箇所あるFXチップ混ざりの高速鍵盤のせいで、PUC難易度は超高いです。しかし、SやAAAなどを取るだけだったら、そこは全部捨てて他のところを光らせる選択を取れば達成への道は見えてきます。このように、18適正の人でもある程度スコアが出せて、PUCを狙いたい人からしてもやりごたえのある譜面として、この曲が適任なのではないかと感じました',
   },
   {
     title: 'GIRIGILI門前雀羅',
-    image: '/public/kadai-song/ポップン/GIRIGILI門前雀羅.png',
+    image: '/kadai-song/ポップン/GIRIGILI門前雀羅.png',
     difficulty: '42',
     remark: 'いい曲選びました。譜面はむずいかも。',
   },
   {
     title: 'Stories',
-    image: '/public/kadai-song/ポップン/Stories.jpg',
+    image: '/kadai-song/ポップン/Stories.jpg',
     difficulty: '47',
     remark: 'いい曲選びました。譜面はむずいかも。',
   },
   {
     title: 'La libertad',
-    image: '/public/kadai-song/DDR/200px-La_libertad.png',
+    image: '/kadai-song/DDR/200px-La_libertad.png',
     difficulty: '15',
     remark: '1曲目は捻りで取ると綺麗に踏めます。',
   },
   {
     title: 'AWAKE',
-    image: '/public/kadai-song/DDR/200px-AWAKE.png',
+    image: '/kadai-song/DDR/200px-AWAKE.png',
     difficulty: '13',
     remark: '2曲目は地力と体力を問われる譜面です。',
   },
   {
     title: 'The Island Song',
-    image: '/public/kadai-song/DDR/jk_thei.jpg',
+    image: '/kadai-song/DDR/jk_thei.jpg',
     difficulty: '14',
     remark: 'コツは曲を良く聴くことです。',
   },
   {
     title: 'STEP MACHINE',
-    image: '/public/kadai-song/DDR/maxresdefault.jpg',
+    image: '/kadai-song/DDR/maxresdefault.jpg',
     difficulty: '14',
     remark: 'コツは曲を良く聴くことです。',
   },
   {
     title: '白虎転生録',
-    image: '/public/kadai-song/ギタフリ/ダウンロード.jpg',
+    image: '/kadai-song/ギタフリ/ダウンロード.jpg',
     difficulty: '7.50',
     remark:
       '白虎転生録…寅年なので選びました。ギターは複雑なリズムが続く、全体的にテクニカルな譜面となっています。特にEXT-G、MAS-Gでは後半に24分オルタも入ってくるため、かなり接続難な譜面となっています。ベースは比較的わかりやすいリズムが多いです。EXT-Bは中速2連オルタがたっぷりあり、MAS-Bだとこれがほぼ全部運指になります。2連オルタや運指オルタの練習になる、いい譜面です。',
   },
   {
     title: '輪廻転生',
-    image: '/public/kadai-song/ギタフリ/gf_gd1.png',
+    image: '/kadai-song/ギタフリ/gf_gd1.png',
     difficulty: '8.20',
     remark:
       '魔法少女に転生したいので選びました。ギター・ベースともにわかりやすい譜面をしているところが多いですが、BPMが速く、かつノーツ数も多いため、かなりの体力譜面となっています。低難度譜面は、BPMこそ早いものの、基本的に難しい運指はないので、右手力向上にいい譜面だと思います。高難度譜面は、8分運指、12分オルタ、16分右手破壊高速オルタなど、高水準の右手力が求められる譜面となっています。特にMAS-Gは、12分は運指譜面になっており、ラストの16分オルタはニギニギする2連オルタなので、虹ネでも苦労する譜面です。(選曲後に"転生"が被っていることに気がつきましたが、皆さんは気がつかないふりをしてください。)',
   },
   {
     title: 'ひまパンケーキメイキング',
-    image: '/public/kadai-song/ドラマニ/ひまパンケーキメイキング.png',
+    image: '/kadai-song/ドラマニ/ひまパンケーキメイキング.png',
     difficulty: '8.5',
     remark:
       '①アーティストのひまわり＊パンチは、「GITADORA OverDrive」から登場した3兄妹のマスコットキャラクターならぬマスコットバンドで、通称「ひまパン」と呼ばれています。GITADORAといえばかっけぇメタルやロックが楽しい機種ですが、ひまわり＊パンチ名義は比較的ポップな楽曲が多いのが特徴です。 DrumManiaは激しい刻みやツーバス譜面が一つの醍醐味ですが、それ以外にも面白い譜面がたくさんあるのをご存知ですか？今回は、昨年から趣向を変えて最新作「GITADORA HIGH-VOLTAGE」で登場した、ややテクニカルなひまわり＊パンチ楽曲を選定させていただきました。MASTER譜面は8.25で全体的に控えめのノーツ密度ですが、フルコンボはランカーでも安定しない難しさがあります。クリアは比較的簡単なのでぜひチャレンジしてみてください。',
   },
   {
     title: '凛として咲く花の如く ～ひなビタ♪ edition～',
-    image: '/public/kadai-song/ドラマニ/凛として咲く花の如く ～ひなビタ♪ edition～.jpg',
+    image: '/kadai-song/ドラマニ/凛として咲く花の如く ～ひなビタ♪ edition～.jpg',
     difficulty: '8.5',
     remark:
       '②コナミ音ゲーで最も有名であろう「凛として咲く花の如く」にはひなビタ♪ボーカル版が存在します。ひなビタ♪はその楽曲が初代GITADORAからかなり積極的に収録され、GITADORA Overdriveでは寸劇付きの単独ひなビタ♪コラボイベントが開催されるなど、なかなかにGITADORAと縁が深い存在です。 今回は、皆さんご存知であろう原曲と適正な難易度や譜面を兼ね備えた本曲を選定させていただきました。 なお、２曲とも専用ムービーがあります。DrumManiaは背景を暗くすると専用ムービーがほとんど見えない悲しい機種ですが、左右どちらかのバスを２回踏むと呼び出せるプレイヤ',
   },
   {
     title: 'ポメグラネイト',
-    image: '/public/kadai-song/jubeat/03030g.png',
+    image: '/kadai-song/jubeat/03030g.png',
     difficulty: '9.1',
     remark:
       'ポメグラネイト:今年から提出制限がS→Aに下がったのを受けて、普段jubeatをやらない人でも赤譜面で提出が狙えるかもしれないレベル8の曲から選びました。ですが、さらに上のスコアを狙うには独特な配置をしっかり理解する必要があり、詰め甲斐のある譜面だと思います。',
   },
   {
     title: 'うらもからだも落花微塵',
-    image: '/public/kadai-song/jubeat/うらもからだも落花微塵.jpg',
+    image: '/kadai-song/jubeat/うらもからだも落花微塵.jpg',
     difficulty: '9.5',
     remark:
       'うらもからだも落花微塵:曲がいいので選びました。イントロとアウトロはスライドや出張が続く配置で忙しいです。難所はやはり中盤の蛇行で、ここでどれだけ失点を抑えられるかが鍵になるかと思います。',
   },
   {
     title: 'サムイウワサ',
-    image: '/public/kadai-song/ノスタルジア/サムイウワサ.png',
+    image: '/kadai-song/ノスタルジア/サムイウワサ.png',
     difficulty: '',
     remark:
       'サムイウワサ:豚乙女さんによるノリノリのロックチューンです。階段や縦連もありノスタルジアの総合譜面とも言える譜面だと思います。',
   },
   {
     title: 'ネリと琥珀糖',
-    image: '/public/kadai-song/ノスタルジア/ネリと琥珀糖.jpg',
+    image: '/kadai-song/ノスタルジア/ネリと琥珀糖.jpg',
     difficulty: '',
     remark:
       'ネリと琥珀糖:sasakure.UKさんによるOp.2の初期曲です。キラキラとしたメロディが特徴的で、指押しでもスライドでも良い練習曲だと思います。',
   },
   {
     title: '後夜祭',
-    image: '/public/kadai-song/ウニ/後夜祭.jpg',
+    image: '/kadai-song/ウニ/後夜祭.jpg',
     difficulty: '12',
     remark:
       '曲自体を聞いて欲しいという思いでこの2曲選びました！曲が多すぎて触れてない人もいると思うので、この機会に是非プレイして欲しいです！',
   },
   {
     title: '《選別》 ～Refuge',
-    image: '/public/kadai-song/ウニ/《選別》 ～Refuge.jpg',
+    image: '/kadai-song/ウニ/《選別》 ～Refuge.jpg',
     difficulty: '13',
     remark:
       '曲自体を聞いて欲しいという思いでこの2曲選びました！曲が多すぎて触れてない人もいると思うので、この機会に是非プレイして欲しいです！',
   },
   {
     title: 'その群青が愛しいようだった',
-    image: '/public/kadai-song/ウニ/その群青が愛しいようだった.jpg',
+    image: '/kadai-song/ウニ/その群青が愛しいようだった.jpg',
     difficulty: '',
     remark:
       'AP狙いも理論値狙いも程よい難易度、チュウニの曲なので人口の多いチュウニプレイヤーで手を出してくれる人がいるかもしれないと思い選びました。',
   },
   {
     title: 'パラボラ',
-    image: '/public/kadai-song/maimai/201210_img4.jpg',
+    image: '/kadai-song/maimai/201210_img4.jpg',
     difficulty: '',
     remark: 'バランスの良い地力譜面なのとmaimaiのいい曲を知って欲しいと思い選びました。',
   },
   {
     title: 'サンドリヨン',
-    image: '/public/kadai-song/diva/サンドリヨン.jpg',
+    image: '/kadai-song/diva/サンドリヨン.jpg',
     difficulty: '',
     remark:
       'サンドリヨンは曲の知名度で選びました。EXTREMEはなんと七帝戦で"実質"初のカンスト曲です！演奏時間がDIVAAC最長のひとつですが、集中力切らさずに頑張っていきましょう！',
   },
   {
     title: 'SING & SMILE',
-    image: '/public/kadai-song/diva/SING & SMILE.jpg',
+    image: '/kadai-song/diva/SING & SMILE.jpg',
     difficulty: '',
     remark:
       'SING & SMILEは完全に個人の好みです。元気の出るミクさん曲、かと思いきやボーカルチェンジに対応してるので、お好きなモジュールをメインにセット、もしくは選曲直後にスライダーのお好きな色を長押ししてみてください！めちゃくちゃかわいいPVにも注目の1曲です！',
   },
   {
     title: '1llusion 0f the FAIRYtALE hARMONY',
-    image: '/public/kadai-song/グルコス/1llusion 0f the FAIRYtALE hARMONY.png',
+    image: '/kadai-song/グルコス/1llusion 0f the FAIRYtALE hARMONY.png',
     difficulty: '11',
     remark:
       '昨年でグルーヴコースターがシリーズ10周年を迎えたので、その記念楽曲のうちの一曲を選びました。8分音符にクリティカルやデュアルスライドがしっかり混ざってきたり、コール合わせのアドリブがあるなど、低難易度ながらもかなり楽しめる譜面かと思います。',
   },
   {
     title: 'taboo tears you up 2017 -GC EDIT-',
-    image: '/public/kadai-song/グルコス/taboo tears you up 2017 -GC EDIT-.png',
+    image: '/kadai-song/グルコス/taboo tears you up 2017 -GC EDIT-.png',
     difficulty: '11',
     remark:
       '今年は2枠なので高難易度から一曲選んでみました。終始16分音符を捌く必要があるので叩き方を工夫したり、体力の配分を考えてみたりするのが攻略の近道かなと思います。グルコス独自の声ネタや演出も楽しんでいただけたら幸いです。  ',
@@ -199,95 +199,95 @@ const KadaiKyokuLists: KadaiKyokuList[] = [
   },
   {
     title: 'ARMAGEΔDON',
-    image: '/public/kadai-song/太鼓の達人/ARMAGEΔDON.jpg',
+    image: '/kadai-song/太鼓の達人/ARMAGEΔDON.jpg',
     difficulty: '',
     remark: '2曲目は偶数、逆手、24分など難所も多く速いです',
   },
   {
     title: '怪物',
-    image: '/public/kadai-song/ダンスラ/怪物.jpg',
+    image: '/kadai-song/ダンスラ/怪物.jpg',
     difficulty: '',
     remark:
       '今回のダンスラッシュ部門はしっかりと技術が問われるであろう譜面にしました。1曲目はPOPSから怪物。メイン機種ではない人が多いと思うので、とっつきやすそうな曲にしました。譜面としては、低速でランニングマンやスポンジボブ、Tステップと基本的な技術が要求される良譜面です。目指せ理論値！',
   },
   {
     title: 'DOWNER ＆ UPPER',
-    image: '/public/kadai-song/ダンスラ/DOWNER ＆ UPPER.jpg',
+    image: '/kadai-song/ダンスラ/DOWNER ＆ UPPER.jpg',
     difficulty: '',
     remark:
       '2曲目はDOWNER & UPPER。今年も1曲のみでの開催だろうと思っていたので、こちらの選曲は悩みました。BPMが(このゲームにとっては)早く、ランニングマンも1段引きですので誤魔化しのききやすい譜面だったりします。まあ、その分スポンジボブが忙しく、普通に難しいんですけどね。',
   },
   {
     title: 'Catch Me If You Can',
-    image: '/public/kadai-song/オンゲキ/Catch Me If You Can.jpg',
+    image: '/kadai-song/オンゲキ/Catch Me If You Can.jpg',
     difficulty: '13',
     remark:
       "ノリノリになれる曲を選びました。知らない曲だな〜って思った方は、ぜひ最初はお家ではなく筐体で聴いてみてください。クラブ気分でLet's オンゲキ！",
   },
   {
     title: 'Daydreama',
-    image: '/public/kadai-song/オンゲキ/Daydreama.jpg',
+    image: '/kadai-song/オンゲキ/Daydreama.jpg',
     difficulty: '13+',
     remark:
       "ノリノリになれる曲を選びました。知らない曲だな〜って思った方は、ぜひ最初はお家ではなく筐体で聴いてみてください。クラブ気分でLet's オンゲキ！",
   },
   {
     title: '13 DONKEYS',
-    image: '/public/kadai-song/wacca/13 DONKEYS.jpg',
+    image: '/kadai-song/wacca/13 DONKEYS.jpg',
     difficulty: '12',
     remark:
       '最近の流行についていけていない老人なので選曲が古いです。WACCAを難しくする要素として認識の難しさがありますが、13 DONKEYSは認識難少なめ。',
   },
   {
     title: 'with U',
-    image: '/public/kadai-song/wacca/with U.jpg',
+    image: '/kadai-song/wacca/with U.jpg',
     difficulty: '13',
     remark:
       'with uは認識難多めとして選曲しました。with uのINFERNOは解禁も優しいので是非やってみてください。青木志貴はいいぞ。',
   },
   {
     title: 'シネマ',
-    image: '/public/kadai-song/プロセカ/シネマ.png',
+    image: '/kadai-song/プロセカ/シネマ.png',
     difficulty: '29',
     remark:
       'シネマはホールド終点や認識難など、プロセカらしさを詰め込んだ総合力が試される譜面となっています。',
   },
   {
     title: '拝啓ドッペルゲンガー',
-    image: '/public/kadai-song/プロセカ/拝啓ドッペルゲンガー.png',
+    image: '/kadai-song/プロセカ/拝啓ドッペルゲンガー.png',
     difficulty: '31',
     remark:
       '拝啓ドッペルゲンガーは前回10分足らずでプロセカ部門を破壊してしまったので、自分の破壊できない曲を入れようと思いました。一緒にAPを目指しましょう。',
   },
   {
     title: 'Sincerity',
-    image: '/public/kadai-song/deemo/deemo.webp',
+    image: '/kadai-song/deemo/deemo.webp',
     difficulty: '9',
     remark:
       ' 混フレやトリルに加え、Deemo特有の技術であるタップスライド移行やスライド持ち替えの技術が多く要求される譜面です。',
   },
   {
     title: 'Toys Etude',
-    image: '/public/kadai-song/deemo/deemo.webp',
+    image: '/kadai-song/deemo/deemo.webp',
     difficulty: '10',
     remark: '完全に担当者の思い入れの強さで選曲しました。難しいので是非挑戦をお待ちしております。',
   },
   {
     title: 'Sweetness and Love(Chapter 4 / Hard 8)',
-    image: '/public/kadai-song/cytus/Sweetness and Love.jpg',
+    image: '/kadai-song/cytus/Sweetness and Love.jpg',
     difficulty: '8',
     remark:
       '3R2といえばコレ！(個人の感想) ラインの遅さやサビの配置から漂う初期譜面らしさがチャームポイントです。',
   },
   {
     title: 'Stardust Sphere(Chapter M / Hard 9)',
-    image: '/public/kadai-song/cytus/Stardust Sphere.jpg',
+    image: '/kadai-song/cytus/Stardust Sphere.jpg',
     difficulty: '9',
     remark: 'リミックス元楽曲の譜面再現が特徴的な総合力譜面です。曲が良い。',
   },
   {
     title: 'NightTheater',
-    image: '/public/kadai-song/bms/NightTheater.jpg',
+    image: '/kadai-song/bms/NightTheater.jpg',
     difficulty: '12',
     remark: '曲, 譜面ともに打鍵の楽しさを感じられるものをチョイスしました。',
   },
@@ -300,14 +300,14 @@ const KadaiKyokuLists: KadaiKyokuList[] = [
   },
   {
     title: 'Easy come, Easy go！',
-    image: '/public/kadai-song/バンドリ/Easy come, Easy go！.png',
+    image: '/kadai-song/バンドリ/Easy come, Easy go！.png',
     difficulty: '',
     remark:
       '初回ということで遠慮して28以上"は"やめておきました。いずれも一癖ある一方で神曲なので、AP目指して楽しくプレイしましょう。',
   },
   {
     title: 'ゴーカ！ ごーかい！？ ファントムシーフ！',
-    image: '/public/kadai-song/バンドリ/ゴーカ！ ごーかい！？ ファントムシーフ！.jpg',
+    image: '/kadai-song/バンドリ/ゴーカ！ ごーかい！？ ファントムシーフ！.jpg',
     difficulty: '',
     remark:
       '初回ということで遠慮して28以上"は"やめておきました。いずれも一癖ある一方で神曲なので、AP目指して楽しくプレイしましょう。',
--- pages/kadai.tsx
+++ pages/kadai.tsx
@@ -1,23 +1,64 @@
-import Cards from '../components/Cards'
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import { Box } from '@mui/system'
+import { KadaiKyokuLists } from '../libs/KadaiKyokuLists'
+import { Card, Typography, CardMedia, CardContent } from '@mui/material'
+
+const Kisyu = [
+  'IIDXSP',
+  'IIDXDP',
+  'SDVX',
+  "pop'n music",
+  'DDRSP',
+  'DDRDP',
+  'GUITARFREAKS',
+  'DrumMania',
+  'jubeat',
+  'ノスタルジア',
+  'CHUNITHM',
+  'maimai',
+  'DIVA',
+  'グルコス',
+  '太鼓の達人',
+  'DANCERUSH STARDOM',
+  'オンゲキ',
+  'WACCA',
+  'プロジェクトセカイ カラフルステージ',
+  'Deemo',
+  'Cytus',
+  'BMS',
+  'バンドリ!ガールズバンドパーティ!',
+]
 
 const Kadai = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1}>
-        <Grid container item spacing={3}>
-          <Cards />
-        </Grid>
-        <Grid container item spacing={3}>
-          <Cards />
-        </Grid>
-        <Grid container item spacing={3}>
-          <Cards />
-        </Grid>
-      </Grid>
-    </Box>
+    <Grid container spacing={2} direction='row' justifyContent='space-evenly' alignItems='center'>
+      {KadaiKyokuLists.map((kadaiKyoku, i) => (
+        <>
+          {/* <Grid item xs={6}> */}
+          <Typography variant='h5'>{Kisyu[i / 2]}</Typography>
+          {/* </Grid> */}
+          <Grid item xs={6} sx={{ maxHeight: 150, minHeight: 150 }}>
+            <Card sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent>
+                  <Typography variant='h6'>{kadaiKyoku.title}</Typography>
+                  <Typography variant='h6'>
+                    {kadaiKyoku.difficulty ? '難易度:' + kadaiKyoku.difficulty : '全難易度'}
+                  </Typography>
+                </CardContent>
+              </Box>
+              <CardMedia
+                component='img'
+                sx={{ maxWidth: 150, maxHeight: 150, minWidth: 150, minHeight: 150 }}
+                image={kadaiKyoku.image}
+                alt={kadaiKyoku.title}
+              />
+            </Card>
+          </Grid>
+        </>
+      ))}
+    </Grid>
   )
 }
 export default Kadai
