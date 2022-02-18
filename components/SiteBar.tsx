1d4c5c36c2dcff3c51fd8386d330010087f3a4c3
ryone9re <ryoichi.kurimoto@gmail.com>
2022-02-18 20:27:07 +0900

    ドロワー作成

--- components/SiteBar.tsx
+++ components/SiteBar.tsx
@@ -1,22 +1,38 @@
 import * as React from 'react'
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material'
+import {
+  AppBar,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+
+import MuiLink from '@mui/material/Link'
 
 import Link from './Link'
 
-const appBarLabel = (label: string) => {
+const DrawerList = () => {
   return (
-    <Toolbar>
-      <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1 }}>
-        <Link href={'/'} target='_blank' underline='none' color={'#ffffff'}>
-          {label}
-        </Link>
-      </Typography>
-      <IconButton edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-        <MenuIcon />
-      </IconButton>
-    </Toolbar>
+    <Box sx={{ width: 250 }} role='presentation'>
+      <List>
+        <MuiLink href='/' color='#fff' underline='none'>
+          <ListItem button key='index'>
+            <ListItemIcon>
+              <HomeOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary='ホーム' />
+          </ListItem>
+        </MuiLink>
+      </List>
+    </Box>
   )
 }
 
@@ -30,10 +46,36 @@ const darkTheme = createTheme({
 })
 
 const SiteBar = () => {
+  const [isOpenDrawer, setDrawerState] = React.useState<boolean>(false)
+
   return (
     <ThemeProvider theme={darkTheme}>
+      <Drawer
+        anchor='right'
+        open={isOpenDrawer}
+        onClose={() => {
+          setDrawerState(false)
+        }}
+      >
+        <DrawerList />
+      </Drawer>
       <AppBar position='static' color='primary'>
-        {appBarLabel('第6回 音ゲー七帝戦')}
+        <Toolbar>
+          <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1 }}>
+            <Link href={'/'} underline='none' color={'#ffffff'}>
+              '第6回 音ゲー七帝戦'
+            </Link>
+          </Typography>
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ mr: 2 }}
+            onClick={() => setDrawerState(!isOpenDrawer)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
       </AppBar>
     </ThemeProvider>
   )
