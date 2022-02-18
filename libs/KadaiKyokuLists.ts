dd90e301959035d0fa30a685f1444c68561b530f
saku411 <vip8124@yahoo.co.jp>
2022-02-18 20:44:44 +0900

    課題曲ページの内容を秘匿

---
+++ libs/KadaiKyokuLists.ts
@@ -0,0 +1,292 @@
+export type KadaiKyokuList = {
+  title: string
+  image: string
+  difficulty: string
+  remark: string
+}
+
+export const KadaiKyokuLists: KadaiKyokuList[] = [
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: '',
+    image: '/kadai-song/coming/images.jpg',
+    difficulty: '',
+    remark: '',
+  },
+  {
+    title: 'シネマ',
+    image: '/kadai-song/プロセカ/シネマ.png',
+    difficulty: '29',
+    remark:
+      'シネマはホールド終点や認識難など、プロセカらしさを詰め込んだ総合力が試される譜面となっています。',
+  },
+  {
+    title: '拝啓ドッペルゲンガー',
+    image: '/kadai-song/プロセカ/拝啓ドッペルゲンガー.png',
+    difficulty: '31',
+    remark:
+      '拝啓ドッペルゲンガーは前回10分足らずでプロセカ部門を破壊してしまったので、自分の破壊できない曲を入れようと思いました。一緒にAPを目指しましょう。',
+  },
+  {
+    title: 'Sincerity',
+    image: '/kadai-song/deemo/deemo.webp',
+    difficulty: '9',
+    remark:
+      ' 混フレやトリルに加え、Deemo特有の技術であるタップスライド移行やスライド持ち替えの技術が多く要求される譜面です。',
+  },
+  {
+    title: 'Toys Etude',
+    image: '/kadai-song/deemo/deemo.webp',
+    difficulty: '10',
+    remark: '完全に担当者の思い入れの強さで選曲しました。難しいので是非挑戦をお待ちしております。',
+  },
+  {
+    title: 'Sweetness and Love(Chapter 4 / Hard 8)',
+    image: '/kadai-song/cytus/Sweetness and Love.jpg',
+    difficulty: '8',
+    remark:
+      '3R2といえばコレ！(個人の感想) ラインの遅さやサビの配置から漂う初期譜面らしさがチャームポイントです。',
+  },
+  {
+    title: 'Stardust Sphere(Chapter M / Hard 9)',
+    image: '/kadai-song/cytus/Stardust Sphere.jpg',
+    difficulty: '9',
+    remark: 'リミックス元楽曲の譜面再現が特徴的な総合力譜面です。曲が良い。',
+  },
+  {
+    title: 'NightTheater',
+    image: '/kadai-song/bms/NightTheater.jpg',
+    difficulty: '12',
+    remark: '曲, 譜面ともに打鍵の楽しさを感じられるものをチョイスしました。',
+  },
+  {
+    title: 'Lepontinia -THE LAST PLANET-',
+    image: '',
+    difficulty: '★17',
+    remark:
+      '2曲目は難易度高めに設定していますが、まだ手が届かないと感じている人もぜひ一度挑戦して将来の目標にしてみてください。',
+  },
+  {
+    title: 'Easy come, Easy go！',
+    image: '/kadai-song/バンドリ/Easy come, Easy go！.png',
+    difficulty: '',
+    remark:
+      '初回ということで遠慮して28以上"は"やめておきました。いずれも一癖ある一方で神曲なので、AP目指して楽しくプレイしましょう。',
+  },
+  {
+    title: 'ゴーカ！ ごーかい！？ ファントムシーフ！',
+    image: '/kadai-song/バンドリ/ゴーカ！ ごーかい！？ ファントムシーフ！.jpg',
+    difficulty: '',
+    remark:
+      '初回ということで遠慮して28以上"は"やめておきました。いずれも一癖ある一方で神曲なので、AP目指して楽しくプレイしましょう。',
+  },
+]
